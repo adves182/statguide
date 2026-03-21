40f63b4dc138217f417203c6c1ea48c58396a9a8
adves182 <adves1313@uib.com>
2026-03-21 16:29:11 +0100

    Add example dataset (PlantGrowth) and documentation

---
+++ R/PlantGrowth.R
@@ -0,0 +1,15 @@
+#' PlantGrowth dataset
+#'
+#' A small dataset measuring plant weight under three treatment groups.
+#'
+#' @format A data frame with 30 rows and 2 variables:
+#' \describe{
+#'   \item{weight}{Numeric outcome variable}
+#'   \item{group}{Factor with three levels: ctrl, trt1, trt2}
+#' }
+#'
+#' @source Built-in R dataset
+#'
+#' @examples
+#' head(PlantGrowth)
+"PlantGrowth"
