290d119bdcd8bb80f72d3a5281c50bb30a6a3ebb
adves182 <adves1313@uib.com>
2026-03-22 22:02:44 +0100

    identify_var_type(): updating roxygen to better the help page with more clear examples

--- R/identify_var_type.R
+++ R/identify_var_type.R
@@ -2,13 +2,15 @@
 #'
 #' Determines whether a variable is numeric or categorical.
 #'
-#' @param x A vector.
+#' @param x A single variable (vector), such as `data$column`.
 #'
 #' @return A character string: "numeric" or "categorical".
 #'
 #' @examples
 #' identify_var_type(1:10)
 #' identify_var_type(factor(c("A", "B")))
+#' identify_var_type(PlantGrowth$weight)
+#' identify_var_type(PlantGrowth$group)
 #'
 #' @export
 identify_var_type <- function(x) {
